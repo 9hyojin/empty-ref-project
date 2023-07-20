069571d5b9134cbadc7a0426709209d939f5f9a1
hyojin_9 <koohyojin1205@gmail.com>
2023-07-20 16:09:39 +0900

    first commit

---
+++ src/main/java/com/teamproject/emptyrefproject/EmptyRefProjectApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.emptyrefproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyRefProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptyRefProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamproject/emptyrefproject/EmptyRefProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamproject.emptyrefproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyRefProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
